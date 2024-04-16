9ea02300745b81896a5e79b273a99801329ad89a
alngrca <162350088+alngrca@users.noreply.github.com>
2024-04-16 20:58:29 -0300

    Github perdido backup

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function aksj() {
+    const firstball = document.getElementById('firstball').style.display='none';
+    const ball = document.getElementById('ball-2').style.display='block';
+    const card = document.getElementById('card').style.display='block'
+
+}
+
+function kkk() {
+    const card = document.getElementById('card').style.display='none';
+    const ball = document.getElementById('ball-2').style.display='none';
+    const firstball = document.getElementById('firstball').style.display='block';
+
+
+}
